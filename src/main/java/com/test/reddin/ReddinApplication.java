494e2ee6d5d37e8c6d7e46197d1362c33ead2dd8
Hana Jiffry <hanajiffryscg@gmail.com>
2022-09-14 23:01:47 +0530

    Initializing

---
+++ src/main/java/com/test/reddin/ReddinApplication.java
@@ -0,0 +1,13 @@
+package com.test.reddin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReddinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/reddin/ReddinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.reddin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
